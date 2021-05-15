fd4f2e1c3ce24d0c218e65c4fbb127139d3159bc
Nick Yang <nick10811@gmail.com>
2021-05-15 14:49:33 +0800

    Initial Commit

---
+++ SwiftUI_Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Sample
+//
+//  Created by Nick Yang on 2021/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
